eb34f953e00edfc4068f7567a16cb65bb39eeece
vlgriga <vladislavgriga@gmail.com>
2018-05-07 17:37:12 +0300

    Lesson 41

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,21 @@
 const passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');  
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done ) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy(
     {
@@ -9,11 +23,19 @@ passport.use(new GoogleStrategy(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done)  =>{
+    (accessToken, refreshToken, profile, done)  => {
       // User.findOrCreate({ googleId: profile.id }, function (err, user) {
       //   return cb(err, user);
       // });
-      console.log(accessToken);
+      User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        if(existingUser){
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id }).save().then( user => 
+            done(null, user));
+        } 
+      }); 
     }
   ));
   
